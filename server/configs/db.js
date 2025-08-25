3f8041bf7f3a3d719901149766be67a5c503feef
FenilVegad <fenilvegad300@gmail.com>
2025-08-25 18:01:51 +0530

    db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-export const ConnectWithDB = () => {
+export const ConnectWithDB = async () => {
   try {
-    mongoose
-      .connect(process.env.MongoDB_Key_Connection)
-      .then(console.log(`MongoDB Connect SuccessFully`))
-      .catch((err) =>
-        console.log(`In The DB Connection Have The Error : ${err}`)
-      );
+    await mongoose.connect(process.env.MongoDB_Key_Connection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB Connected Successfully");
   } catch (err) {
-    console.log(`In The DB Connection Have The Error : ${err}`);
+    console.error("❌ In The DB Connection Have The Error :", err.message);
+    process.exit(1);
   }
 };
